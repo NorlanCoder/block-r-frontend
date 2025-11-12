7d981cfc3518512d26dfcf3fce8528f0e2d83032
NorlanCoder <fabien.amoussou20@gmail.com>
2025-11-12 21:19:26 +0100

    fix: New cart printed format

--- src/components/Agent/CardPreview.tsx
+++ src/components/Agent/CardPreview.tsx
@@ -1,67 +1,177 @@
 import React from "react";
+import { QRCodeSVG } from "qrcode.react";
 import { apiUrlBase } from "../../api";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { FaCircle, FaFileAlt, FaHome, FaInfoCircle, FaLocationArrow, FaMapPin, FaPhone, FaUser } from "react-icons/fa";
 
 interface CardPreviewProps {
-  militant: any; // tu peux remplacer par MilitantType si tu veux typé fort
+  militant: any;
 }
 
 const CardPreview: React.FC<CardPreviewProps> = ({ militant }) => {
-    console.log(militant)
-    const app = useSelector((state: RootState) => state.appReducer);
+  const app = useSelector((state: RootState) => state.appReducer);
+  
+  // Formatage du numéro de carte avec padding à 6 chiffres
+  const formatCardNumber = (num: string | number | undefined): string => {
+    if (!num) return "000000";
+    const numStr = String(num);
+    return numStr.padStart(6, "0");
+  };
+
+  // Récupération des noms depuis les IDs
+  const communeName = app.communes.find((c) => String(c.code_com) === String(militant.commune_id))?.lib_com || "";
+  const departementName = app.departements.find((d) => String(d.code_dep) === String(militant.departement_id))?.lib_dep || "";
+  const circonscriptionName = app.circonscriptions.find((c) => String(c.code_circ) === String(militant.circonscription_id))?.lib_circ || "";
+
+  // Génération du contenu du QR code (identique au PHP)
+  const qrCodeValue = `${militant.reference_carte || formatCardNumber(militant.reference_carte)}|${militant.nom || ""}|${militant.prenom || ""}|${circonscriptionName}|${communeName}|https://www.pepsup.com/login`;
+
+  // Numéro de carte formaté
+  const numCarteFormat = formatCardNumber(militant.reference_carte);
+
   return (
     <div className="flex items-center justify-center m-14">
+      <div
+        id="card-preview"
+        className="card-preview relative"
+        style={{
+          width: "86mm",
+          height: "54mm",
+          background: "#1ccccc",
+          border: "1px solid #000",
+          boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
+          margin: 0,
+          padding: 0,
+          position: "relative",
+        }}
+      >
+        {/* QR Code positionné absolument comme dans le PHP */}
         <div
-            id="card-preview"
-            className="card-preview relative border rounded shadow-md bg-white flex flex-row justify-between"
-            style={{
-                width: "86mm",
-                height: "54mm",
-                border: "1px solid #000",
-                boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
-            }}
+          style={{
+            position: "absolute",
+            top: "40mm",
+            left: "35mm",
+            backgroundColor: "#FFFFFF",
+            border: "none",
+            width: "10mm",
+            height: "10mm",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-            <div className="text-gray-600" style={{ width: "80%", maxWidth: "80%" }}>
-                <div className="flex gap-2 items-center">
-                    <h3 className="text-[11px] pl-10">{militant.nom}</h3>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <p className="text-[11px] pl-10">{militant.prenom}</p>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <p className="text-[11px] pl-10">{militant.profession}</p>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <p className="text-[11px] pl-10">{militant.adresse}</p>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <p className="text-[11px] pl-10">{app.communes.find((s) => s.code_com === militant.commune_id)?.lib_com}</p>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <p className="text-[11px] pl-10">{app.departements.find((d) => d.code_dep === militant.departement_id)?.lib_dep}</p>
-                </div>
-                {/* <div className="flex gap-2 items-center">
-                    <p className="text-[11px] pl-10">{app.circonscriptions.find((c) => c.code_circ === militant.circonscription_id)?.lib_circ}</p>
-                </div> */}
-                <div className="flex mb-2 gap-2 items-center">
-                    <p className="text-[11px] pl-10">{militant.telephone}</p>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <img
-                        style={{ width: "80px", height: "95px", border: "1px solid #000", objectFit: "cover" }}
-                        src={militant.photo ? apiUrlBase + militant.photo : "/default-avatar.png"}
-                        alt="Photo"
-                        width={80}
-                        height={95}
-                        className="mb-2"
-                    />
-                </div>
-            </div>
+          <QRCodeSVG
+            value={qrCodeValue}
+            size={100}
+            level="L"
+            bgColor="#FFFFFF"
+            fgColor="#000000"
+            style={{ width: "10mm", height: "10mm" }}
+          />
         </div>
+
+        {/* Table structure comme dans le PHP */}
+        <table
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "0",
+            borderCollapse: "collapse",
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          <tbody>
+            <tr>
+              {/* Colonne gauche avec les informations */}
+              <td
+                style={{
+                  width: "70%",
+                  rowSpan: 2,
+                  paddingLeft: "15px",
+                  lineHeight: "13px",
+                  paddingTop: "48px",
+                  fontSize: "8px",
+                  letterSpacing: "2px",
+                  verticalAlign: "top",
+                }}
+              >
+                {/* Nom */}
+                <span style={{ marginTop: "1px", fontSize: "10px" }}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{militant.nom || ""}
+                </span>
+                <br />
+                {/* Prénom */}
+                <span style={{ marginTop: "2px", fontSize: "10px" }}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{militant.prenom || ""}
+                </span>
+                <br />
+                {/* Profession */}
+                <span style={{ marginTop: "3px", fontSize: "10px" }}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{militant.profession || ""}
+                </span>
+                <br />
+                {/* Adresse */}
+                <span style={{ marginTop: "5px", fontSize: "10px" }}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{militant.adresse || ""}
+                </span>
+                <br />
+                {/* Circonscription */}
+                <span style={{ marginTop: "7px", fontSize: "9px" }}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{circonscriptionName}
+                </span>
+                <br />
+                {/* Commune / Département */}
+                <span style={{ fontSize: "9px", marginTop: "8px" }}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{communeName}/{departementName}
+                </span>
+                <br />
+                {/* Contact */}
+                <span style={{ fontSize: "10px", marginTop: "9px" }}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;00229 {militant.telephone || ""}
+                </span>
+                <br />
+              </td>
+              {/* Colonne droite - Numéro de carte */}
+              <td
+                align="center"
+                style={{
+                  paddingTop: "13px",
+                  fontSize: "10px",
+                }}
+              >
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{numCarteFormat}
+              </td>
+            </tr>
+            <tr>
+              {/* Colonne droite - Photo */}
+              <td
+                width=""
+                align="center"
+                valign="top"
+                style={{
+                  paddingTop: "7px",
+                  paddingBottom: "20px",
+                  paddingLeft: "1px",
+                  paddingRight: "1px",
+                }}
+              >
+                <img
+                  src={militant.photo ? (typeof militant.photo === 'string' && militant.photo.startsWith('http') ? militant.photo : apiUrlBase + militant.photo) : "/default-avatar.png"}
+                  alt="Photo"
+                  style={{
+                    width: "70px",
+                    height: "auto",
+                    maxWidth: "70px",
+                    border: "1px solid black",
+                    objectFit: "cover",
+                  }}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
